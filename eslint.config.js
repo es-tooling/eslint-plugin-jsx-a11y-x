83b219511e88dcc671548b322d579208e94a14e3
James Garbutt <43081j@users.noreply.github.com>
2024-10-16 15:51:11 +0900

    chore: initial commit

---
+++ eslint.config.js
@@ -0,0 +1,17 @@
+const {configs: eslintConfigs} = require('@eslint/js');
+const {configs: tseslintConfigs} = require('typescript-eslint');
+const pluginRecommended = require('eslint-plugin-eslint-plugin/configs/recommended');
+
+module.exports = [
+  {
+    ...eslintConfigs.recommended,
+    files: ['src/**/*.ts'],
+  },
+  {
+    rules: {
+      'max-len': 'error'
+    }
+  },
+  pluginRecommended,
+  ...tseslintConfigs.strict
+];
